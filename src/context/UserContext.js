675ad4bd42591e2e3b733f65b7fb8a786ef28949
torastrengel <heikki@heikki.codes>
2023-01-29 15:29:10 +0200

    Default value not null

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-export const UserContext = createContext(null);
+export const UserContext = createContext({});
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState({
