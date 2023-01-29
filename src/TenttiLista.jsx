064758773393464bed58e5082644721cbf1f467c
torastrengel <heikki@heikki.codes>
2023-01-29 15:28:24 +0200

    Wrap to trycatch and don't return the error

--- src/TenttiLista.jsx
+++ src/TenttiLista.jsx
@@ -7,23 +7,26 @@ import { TenttiContext } from './context/TenttiContext';
 import Button from '@mui/material/Button';
 
 const TenttiLista = () => {
-  const { dispatch, tentti } = useContext(TenttiContext);
+  const { tentti, dispatch } = useContext(TenttiContext);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const haeTentit = async () => {
-      const { data } = await axios.get('https://localhost:3001/tentit');
-      if (data.success) {
-        dispatch({
-          type: 'HAE_KAIKKI_TENTIT',
-          payload: {
-            tentit: data.results,
-          },
-        });
+      try {
+        const { data } = await axios.get('https://localhost:3001/tentit');
+        if (data.success) {
+          dispatch({
+            type: 'HAE_KAIKKI_TENTIT',
+            payload: {
+              tentit: data.results,
+            },
+          });
+        }
+      } catch (error) {
+        console.error(error.message);
+        <h2>Tenttien haussa virhe!</h2>;
+      } finally {
         setIsLoading(false);
-      } else {
-        setIsLoading(false);
-        return <h1>Tenttien haussa virhe!</h1>;
       }
     };
     haeTentit();
@@ -36,13 +39,18 @@ const TenttiLista = () => {
 
   return (
     <div>
+      {console.log(tentti)}
       <h1>Valittavissa olevat tentit:</h1>
       <div className="tenttinapit-container">
-        {tentti.tenttilista?.map((item) => (
-          <Link key={item.id} to={`/tentit/${item.id}`}>
-            <Button>{item.nimi}</Button>
-          </Link>
-        ))}
+        {tentti && tentti.tenttilista ? (
+          tentti.tenttilista.map((item) => (
+            <Link key={item.id} to={`/tentit/${item.id}`}>
+              <Button>{item.nimi}</Button>
+            </Link>
+          ))
+        ) : (
+          <h2>Tenttejä ei löytynyt</h2>
+        )}
       </div>
     </div>
   );
